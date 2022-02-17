0ff56b3e322e1d0502026688ebcec9084479087e
Aldrin-Alocious <99838573+Aldrin-Alocious@users.noreply.github.com>
2022-02-17 18:54:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ import streamlit as st
 import joblib
 model=joblib.load('IRIS CLASSIFIER')
 st.title('IRIS CLASSIFIER')
-ip1=st.slider_input('Enter Sepal Length:', min_value=0, max_value=10, step=0.1, format=%f)
-ip2=st.slider_input('Enter Sepal Width:', min_value=0, max_value=10, step=0.1, format=%f)
-ip3=st.slider_input('Enter Petal Length:', min_value=0, max_value=10, step=0.1, format=%f)
-ip4=st.slider_input('Enter Petal Width:', min_value=0, max_value=10, step=0.1, format=%f)
+ip1=st.slider('Enter Sepal Length:', min_value=0, max_value=10, step=0.1, format=%f)
+ip2=st.slider('Enter Sepal Width:', min_value=0, max_value=10, step=0.1, format=%f)
+ip3=st.slider('Enter Petal Length:', min_value=0, max_value=10, step=0.1, format=%f)
+ip4=st.slider('Enter Petal Width:', min_value=0, max_value=10, step=0.1, format=%f)
 op=model.predict([ip1,ip2,ip3,ip4])
 if st.button('Predict'):
   st.title(op[0])
