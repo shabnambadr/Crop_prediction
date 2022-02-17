f70c8a49a45218a14fa66a67b1956c677ed676f0
Aldrin-Alocious <99838573+Aldrin-Alocious@users.noreply.github.com>
2022-02-17 20:00:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ st.subheader('Petal Length')
 ip3=st.slider('Enter a value between 0 & 10:', min_value=0.0, max_value=10.0, step=0.1, key=3)
 st.subheader('Petal Length')
 ip4=st.slider('Enter a value between 0 & 10:', min_value=0.0, max_value=10.0, step=0.1, key=4)
-op=model.predict([ip1,ip2,ip3,ip4])
-if st.button('Predict'):
-  st.title(op[0])
+#op=model.predict([ip1,ip2,ip3,ip4])
+#if st.button('Predict'):
+ # st.title(op[0])
+print(ip1,ip2,ip4)
