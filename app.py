ef9db70a7696b862138a8b8d135e77a44172c18d
Aldrin-Alocious <99838573+Aldrin-Alocious@users.noreply.github.com>
2022-02-17 19:55:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,13 @@ import streamlit as st
 import joblib
 st.title('IRIS CLASSIFIER')
 st.subheader('Sepal Length')
-ip1=st.slider('Enter a value between 0 & 10:', min_value=0.0, max_value=10.0, step=0.1)
+ip1=st.slider('Enter a value between 0 & 10:', min_value=0.0, max_value=10.0, step=0.1, key=1)
 st.subheader('Sepal Width')
-ip2=st.slider('Enter a value between 0 & 10:', min_value=0.0, max_value=10.0, step=0.1)
+ip2=st.slider('Enter a value between 0 & 10:', min_value=0.0, max_value=10.0, step=0.1, key=2)
 st.subheader('Petal Length')
-ip3=st.slider('Enter a value between 0 & 10:', min_value=0.0, max_value=10.0, step=0.1)
+ip3=st.slider('Enter a value between 0 & 10:', min_value=0.0, max_value=10.0, step=0.1, key=3)
 st.subheader('Petal Length')
-ip4=st.slider('Enter a value between 0 & 10:', min_value=0.0, max_value=10.0, step=0.1)
+ip4=st.slider('Enter a value between 0 & 10:', min_value=0.0, max_value=10.0, step=0.1, key=4)
 op=model.predict([ip1,ip2,ip3,ip4])
 if st.button('Predict'):
   st.title(op[0])
