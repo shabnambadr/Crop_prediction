fc589bf381d3972246d792b1fd55484576a3ee67
Aldrin-Alocious <99838573+Aldrin-Alocious@users.noreply.github.com>
2022-02-17 19:04:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,14 @@ import streamlit as st
 import joblib
 model=joblib.load('IRIS CLASSIFIER')
 st.title('IRIS CLASSIFIER')
-ip1=st.slider('Enter Sepal Length:', min_value=0, max_value=10, step=0.1, format=%f)
-ip2=st.slider('Enter Sepal Width:', min_value=0, max_value=10, step=0.1, format=%f)
-ip3=st.slider('Enter Petal Length:', min_value=0, max_value=10, step=0.1, format=%f)
-ip4=st.slider('Enter Petal Width:', min_value=0, max_value=10, step=0.1, format=%f)
+st.subheader('Sepal Length')
+ip1=st.slider('Enter a value between 0 & 10:', min_value=0, max_value=10, step=0.1)
+st.subheader('Sepal Width')
+ip2=st.slider('Enter a value between 0 & 10:', min_value=0, max_value=10, step=0.1)
+st.subheader('Petal Length')
+ip3=st.slider('Enter a value between 0 & 10:', min_value=0, max_value=10, step=0.1)
+st.subheader('Petal Length')
+ip4=st.slider('Enter a value between 0 & 10:', min_value=0, max_value=10, step=0.1)
 op=model.predict([ip1,ip2,ip3,ip4])
 if st.button('Predict'):
   st.title(op[0])
