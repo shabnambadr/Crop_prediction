8cddc57a949e0750d72d2efd4f266c0e92f8d64b
Aldrin-Alocious <99838573+Aldrin-Alocious@users.noreply.github.com>
2022-02-17 20:42:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import joblib
+import pandas as pd
+from sklearn import datasets
 st.title('IRIS CLASSIFIER')
 st.subheader('Sepal Length')
 ip1=st.slider('Enter a value between 0 & 10:', min_value=0.0, max_value=10.0, step=0.1, key=1)
@@ -9,6 +11,14 @@ st.subheader('Petal Length')
 ip3=st.slider('Enter a value between 0 & 10:', min_value=0.0, max_value=10.0, step=0.1, key=3)
 st.subheader('Petal Length')
 ip4=st.slider('Enter a value between 0 & 10:', min_value=0.0, max_value=10.0, step=0.1, key=4)
-#op=model.predict([ip1,ip2,ip3,ip4])
+iris=datasets.load_iris()
+x=iris.data
+y=iris.target
+df=pd.DataFrame(iris.data)
+df['Target']=iris.target
+from sklearn.neighbors import KNeighborsClassifier
+model=KNeighborsClassifier()
+model.fit(x,y)
+op=model.predict([ip1,ip2,ip3,ip4])
 if st.button('Predict'):
-  st.title(ip1,ip2,ip3,ip4)
+  st.title(op[0])
