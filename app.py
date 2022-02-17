4c96a437105dff16d23ddceaa6f72bb2e3dfd487
Aldrin-Alocious <99838573+Aldrin-Alocious@users.noreply.github.com>
2022-02-17 23:29:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,15 @@ st.subheader('Petal Length')
 ip3=st.slider('Enter a value between 0 & 10:', min_value=0.0, max_value=10.0, step=0.1, key=3)
 st.subheader('Petal Length')
 ip4=st.slider('Enter a value between 0 & 10:', min_value=0.0, max_value=10.0, step=0.1, key=4)
-model=joblib.load('iris')
+iris=datasets.load_iris()
+x=iris.data
+y=iris.target
+df=pd.DataFrame(iris.data)
+df['Target']=iris.target
+from sklearn.neighbors import KNeighborsClassifier
+model=KNeighborsClassifier()
+model.fit(x,y)
+joblib.dump(model,'IRIS classifier')
 op=model.predict([[ip1,ip2,ip3,ip4]])
 if op[0]==0:
   out="Iris setosa"
