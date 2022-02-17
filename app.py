8e26a05ae5bc1e53b508d75fd012c05853022e2e
Aldrin-Alocious <99838573+Aldrin-Alocious@users.noreply.github.com>
2022-02-17 18:41:47 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import joblib
+model=joblib.load('IRIS CLASSIFIER')
+st.title('IRIS CLASSIFIER')
+ip1=st.slider_input('Enter Sepal Length:', min_value=0, max_value=10, step=0.1, format=%f)
+ip2=st.slider_input('Enter Sepal Width:', min_value=0, max_value=10, step=0.1, format=%f)
+ip3=st.slider_input('Enter Petal Length:', min_value=0, max_value=10, step=0.1, format=%f)
+ip4=st.slider_input('Enter Petal Width:', min_value=0, max_value=10, step=0.1, format=%f)
+op=model.predict([ip1,ip2,ip3,ip4])
+if st.button('Predict'):
+  st.title(op[0])
