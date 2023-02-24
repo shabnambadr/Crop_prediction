5b2930449c68894fcba5688e54b432863f5a1d1e
Varadh2001 <126092305+Varadh2001@users.noreply.github.com>
2023-02-24 11:31:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,44 +1,32 @@
-from keras.models import load_model
-from PIL import Image, ImageOps #Install pillow instead of PIL
-import numpy as np
-import streamlit as st
-st.write('hi')
-# Disable scientific notation for clarity
-np.set_printoptions(suppress=True)
+from flask import Flask, render_template, request
+import pickle
 
-# Load the model
-model = load_model('keras_model.h5', compile=False)
+# Load trained model from file
+with open('model.pkl', 'rb') as f:
+    model = pickle.load(f)
 
-# Load the labels
-class_names = open('labels.txt', 'r').readlines()
+# Set up Flask application
+app = Flask(__name__)
 
-# Create the array of the right shape to feed into the keras model
-# The 'length' or number of images you can put into the array is
-# determined by the first position in the shape tuple, in this case 1.
-data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+# Define route for home page
+@app.route("/")
+def home():
+    return render_template('index.html')
 
-# Replace this with the path to your image
-image = Image.open(st.file_uploader("Upload the test image:")).convert('RGB')
+# Define route for prediction page
+@app.route("/predict", methods=['POST'])
+def predict():
+    # Get input from web form
+    day = request.form['day']
+    item_id = request.form['item_id']
 
-#resize the image to a 224x224 with the same strategy as in TM2:
-#resizing the image to be at least 224x224 and then cropping from the center
-size = (224, 224)
-image = ImageOps.fit(image, size, Image.Resampling.LANCZOS)
+    # Use model to make prediction
+    X_new = [[day, item_id]]
+    y_new = model.predict(X_new)
 
-#turn the image into a numpy array
-image_array = np.asarray(image)
+    # Render prediction page with predicted sales
+    return render_template('predict.html', sales=y_new[0])
 
-# Normalize the image
-normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
-
-# Load the image into the array
-data[0] = normalized_image_array
-
-# run the inference
-prediction = model.predict(data)
-index = np.argmax(prediction)
-class_name = class_names[index]
-confidence_score = prediction[0][index]
-
-st.write('Class:', class_name, end='')
-st.write('Confidence score:', confidence_score)
+# Run Flask application
+if __name__ == '_main_':
+    app.run(debug=True,port=5001)
