98faf01a8837c5a11e9a593b1811cc4cf6eadc59
Varadh2001 <126092305+Varadh2001@users.noreply.github.com>
2023-02-25 13:35:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+import scikit_learn as sklearn
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
