ae09f02b8f2616e154ba556235f938af59cf2a82
Varadh2001 <126092305+Varadh2001@users.noreply.github.com>
2023-02-25 13:23:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,6 @@ import streamlit as st
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
-from flask import Flask, render_template, request
-import pickle
 
 # Load the sales data
 sales_df = pd.read_csv('sales_data.csv')
@@ -51,32 +49,4 @@ y_new = model.predict(X_new)
 print('Predicted sales: ', y_new[0])
 
 
-# Load trained model from file
-with open('model.pkl', 'rb') as f:
-    model = pickle.load(f)
 
-# Set up Flask application
-app = Flask(__name__)
-
-# Define route for home page
-@app.route("/")
-def home():
-    return render_template('index.html')
-
-# Define route for prediction page
-@app.route("/predict", methods=['POST'])
-def predict():
-    # Get input from web form
-    day = request.form['day']
-    item_id = request.form['item_id']
-
-    # Use model to make prediction
-    X_new = [[day, item_id]]
-    y_new = model.predict(X_new)
-
-    # Render prediction page with predicted sales
-    return render_template('predict.html', sales=y_new[0])
-
-# Run Flask application
-if __name__ == '_main_':
-    app.run(debug=True,port=5001)
