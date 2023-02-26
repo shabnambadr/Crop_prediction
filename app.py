c239e686c3aa7f64cd8abdbc49c13428bff106c1
shabnambadr <70138329+shabnambadr@users.noreply.github.com>
2023-02-26 11:23:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,8 +15,8 @@ encoded_y = pd.DataFrame(encoded_y, columns=enc.get_feature_names_out(['yield'])
 data = pd.concat([data, encoded_y], axis=1)                                                                
                                                                                                            
 # Creating the input and output variables for the regression model                                         
-X = data[['humidity', 'temperature', 'rainfall']]                                                          
-y = data.drop(['humidity', 'temperature', 'rainfall', 'yield'], axis=1)                                    
+X = data[['humidity', 'temperature', 'rainfall', 'ph']]                                                          
+y = data.drop(['humidity', 'temperature', 'rainfall', 'ph', 'yield'], axis=1)                                    
                                                                                                            
 # Creating the regression model and fitting it to the input and output variables                           
 model = LinearRegression()                                                                                 
@@ -31,15 +31,16 @@ st.sidebar.header('Input Parameters')
 humidity = st.sidebar.slider('Humidity', 0.0, 100.0, 50.0, 1.0)
 temperature = st.sidebar.slider('Temperature', 0.0, 50.0, 25.0, 1.0)
 rainfall = st.sidebar.slider('Rainfall', 0.0, 300.0, 100.0, 1.0)
+ph = st.sidebar.slider('pH', 3.0, 10.0, 7.0, 0.1)
 
 # Create a button to trigger prediction                                                                    
 if st.sidebar.button('Predict Yield'):                                                                     
     # Predicting the yield for the input parameters using the trained model                                
-    new_data = pd.DataFrame({'humidity': [humidity], 'temperature': [temperature], 'rainfall': [rainfall]})
+    new_data = pd.DataFrame({'humidity': [humidity], 'temperature': [temperature], 'rainfall': [rainfall], 'ph': [ph]})
     predicted_yield = model.predict(new_data)                                                              
                                                                                                              
     # Dropping the additional column from the encoded data                                                 
-    encoded_y = data.drop(['humidity', 'temperature', 'rainfall', 'yield'], axis=1)                        
+    encoded_y = data.drop(['humidity', 'temperature', 'rainfall', 'ph', 'yield'], axis=1)                        
                                                                                                              
     # Inverse transforming the predicted yield to get the original crop name                                
     inverse_predicted_yield = enc.inverse_transform(predicted_yield[:, :22])                                
@@ -47,6 +48,7 @@ if st.sidebar.button('Predict Yield'):
     # Displaying the predicted yield                                                                         
     st.header('Predicted Yield')
     st.write('The crop with the maximum yield based on the given parameters is:', inverse_predicted_yield[0][0])
+
  
 
 
