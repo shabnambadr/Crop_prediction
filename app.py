557cd1d30e8410378dad3ce827ab467226de236e
shabnambadr <70138329+shabnambadr@users.noreply.github.com>
2023-02-26 11:16:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression                                                          
 from sklearn.preprocessing import OneHotEncoder                                                            
 import pickle                                                                                              
-                                                                                                           
+import streamlit as st                                                                                     
+
 # Load dataset from CSV file                                                                               
 data = pd.read_csv("Crop_recommendation.csv")                                                         
                                                                                                            
@@ -25,23 +26,28 @@ model.fit(X, y)
 with open('crop_yield_prediction_model.pkl', 'wb') as f:                                                   
     pickle.dump(model, f)                                                                                  
                                                                                                            
-# Get input from user for new test data                                                                    
-new_data = pd.DataFrame(columns=['humidity', 'temperature', 'rainfall'])                                   
-new_data.loc[0] = [float(input("Enter humidity value: ")),                                                 
-                   float(input("Enter temperature value: ")),                                              
-                   float(input("Enter rainfall value: "))]                                                 
-                                                                                                           
-# Predicting the yield for the input parameters using the trained model                                    
-predicted_yield = model.predict(new_data)                                                                  
-                                                                                                           
-# Dropping the additional column from the encoded data                                                     
-encoded_y = data.drop(['humidity', 'temperature', 'rainfall', 'yield'], axis=1)                            
-                                                                                                           
-# Inverse transforming the predicted yield to get the original crop name                                   
-inverse_predicted_yield = enc.inverse_transform(predicted_yield[:, :22])                                   
-                                                                                                           
-# Printing the predicted yield                                                                             
-print('The crop with the maximum yield based on the given parameters is:', inverse_predicted_yield[0][0])  
+# Add input widgets for new test data                                                                    
+st.sidebar.header('Input Parameters')
+humidity = st.sidebar.slider('Humidity', 0.0, 100.0, 50.0, 1.0)
+temperature = st.sidebar.slider('Temperature', 0.0, 50.0, 25.0, 1.0)
+rainfall = st.sidebar.slider('Rainfall', 0.0, 300.0, 100.0, 1.0)
+
+# Create a button to trigger prediction                                                                    
+if st.sidebar.button('Predict Yield'):                                                                     
+    # Predicting the yield for the input parameters using the trained model                                
+    new_data = pd.DataFrame({'humidity': [humidity], 'temperature': [temperature], 'rainfall': [rainfall]})
+    predicted_yield = model.predict(new_data)                                                              
+                                                                                                             
+    # Dropping the additional column from the encoded data                                                 
+    encoded_y = data.drop(['humidity', 'temperature', 'rainfall', 'yield'], axis=1)                        
+                                                                                                             
+    # Inverse transforming the predicted yield to get the original crop name                                
+    inverse_predicted_yield = enc.inverse_transform(predicted_yield[:, :22])                                
+                                                                                                             
+    # Displaying the predicted yield                                                                         
+    st.header('Predicted Yield')
+    st.write('The crop with the maximum yield based on the given parameters is:', inverse_predicted_yield[0][0])
+ 
 
 
 
