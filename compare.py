b37fa0360e31239cfd2c769f458b0373a1c09408
shabnambadr <70138329+shabnambadr@users.noreply.github.com>
2023-03-14 21:54:30 +0530

    Update and rename app2.py to compare.py

---
+++ compare.py
@@ -0,0 +1,45 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+import matplotlib.pyplot as plt
+import pickle
+
+# Load data
+data = pd.read_csv('Crop_recommendation.csv')
+
+# Load trained models
+with open('rf_model.pkl', 'rb') as f:
+    rf_model = pickle.load(f)
+
+with open('lr_model.pkl', 'rb') as f:
+    lr_model = pickle.load(f)
+
+# Get user input
+rainfall = st.sidebar.slider('Rainfall', 0, 500, 100)
+ph = st.sidebar.slider('pH', 3.5, 9.0, 7.0)
+temperature = st.sidebar.slider('Temperature', 0, 50, 25)
+n_content = st.sidebar.slider('N Content', 0, 200, 100)
+p_content = st.sidebar.slider('P Content', 0, 200, 100)
+k_content = st.sidebar.slider('K Content', 0, 200, 100)
+
+# Make predictions
+rf_prediction = rf_model.predict([[rainfall, ph, temperature, n_content, p_content, k_content]])
+lr_prediction = lr_model.predict([[rainfall, ph, temperature, n_content, p_content, k_content]])
+
+# Calculate accuracy
+rf_accuracy = r2_score(y_true, rf_prediction)
+lr_accuracy = r2_score(y_true, lr_prediction)
+
+# Plot bar chart
+fig, ax = plt.subplots()
+model_names = ['Random Forest', 'Linear Regression']
+accuracy_values = [rf_accuracy, lr_accuracy]
+ax.bar(model_names, accuracy_values)
+ax.set_title('Accuracy Comparison')
+ax.set_xlabel('Model')
+ax.set_ylabel('Accuracy')
+st.pyplot(fig)
+
