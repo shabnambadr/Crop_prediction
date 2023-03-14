ad887eac63430c1cac89f64eb0df6b5b5835bdfa
shabnambadr <70138329+shabnambadr@users.noreply.github.com>
2023-03-14 21:11:44 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,41 +1,40 @@
 import streamlit as st
 import pandas as pd
+from sklearn.datasets import load_iris
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import accuracy_score
-import matplotlib.pyplot as plt
 
-# Load data
-data = pd.read_csv('Crop_recommendation.csv')
+# Load the iris dataset
+iris = load_iris()
+X, y = iris.data, iris.target
 
-# Split data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(
-    data[['ph', 'humidity', 'rainfall', 'temperature', 'N', 'P', 'K']],
-    data['yield'],
-    test_size=0.3,
-    random_state=42
-)
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
-# Linear regression model
-lr = LinearRegression()
+# Create and train the logistic regression model
+lr = LogisticRegression()
 lr.fit(X_train, y_train)
-
-# Random forest model
-rf = RandomForestRegressor(n_estimators=100)
-rf.fit(X_train, y_train)
-
-# Prediction and accuracy evaluation
 lr_pred = lr.predict(X_test)
-rf_pred = rf.predict(X_test)
+lr_acc = accuracy_score(y_test, lr_pred)
 
-lr_accuracy = accuracy_score(y_test, lr_pred.round())
-rf_accuracy = accuracy_score(y_test, rf_pred.round())
+# Create and train the decision tree model
+dt = DecisionTreeClassifier()
+dt.fit(X_train, y_train)
+dt_pred = dt.predict(X_test)
+dt_acc = accuracy_score(y_test, dt_pred)
 
-# Plot pie chart comparing accuracies
-fig, ax = plt.subplots(figsize=(5,5))
-accuracy = [lr_accuracy, rf_accuracy]
-models = ['Linear Regression', 'Random Forest']
-ax.pie(accuracy, labels=models, autopct='%1.2f%%')
-ax.set_title('Model Accuracy')
-st.pyplot(fig)
+# Display the results in a pie chart
+data = {'Model': ['Logistic Regression', 'Decision Tree'],
+        'Accuracy': [lr_acc, dt_acc]}
+df = pd.DataFrame(data)
+fig = px.pie(df, values='Accuracy', names='Model')
+st.plotly_chart(fig)
+
+# Display the higher accuracy model
+if lr_acc > dt_acc:
+    st.write('Logistic Regression has higher accuracy: ', lr_acc)
+else:
+    st.write('Decision Tree has higher accuracy: ', dt_acc)
+
