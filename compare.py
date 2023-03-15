f641076f35d4388b23be4deaade08ba43c342157
shabnambadr <70138329+shabnambadr@users.noreply.github.com>
2023-03-15 08:54:14 +0530

    Update compare.py

--- compare.py
+++ compare.py
@@ -11,13 +11,8 @@ import joblib
 data = pd.read_csv('Crop_recommendation.csv')
 
 # Load trained models
-rf_model = RandomForestRegressor()
 rf_model = joblib.load('rf_model.pkl')
-lr_model = LinearRegression()
 lr_model = joblib.load('lr_model.pkl')
-
-# Define y_true variable
-y_true = data['yield']
 
 # Get user input
 humidity = st.sidebar.slider('Humidity', 0.0, 100.0, 50.0, 1.0)
@@ -32,22 +27,9 @@ K = st.sidebar.slider('Potassium', 0.0, 200.0, 75.0, 1.0)
 rf_prediction = rf_model.predict([[humidity, temperature, rainfall, ph, N, P, K]])
 lr_prediction = lr_model.predict([[humidity, temperature, rainfall, ph, N, P, K]])
 
-# Print predictions
-st.write('Random Forest prediction:', rf_prediction[0])
-st.write('Linear Regression prediction:', lr_prediction[0])
+# Display predicted values to user
+st.write('Random Forest predicted yield:', rf_prediction[0])
+st.write('Linear Regression predicted yield:', lr_prediction[0])
 
-# Calculate accuracy
-rf_accuracy = r2_score(y_true, rf_prediction)
-lr_accuracy = r2_score(y_true, lr_prediction)
-
-# Plot bar chart
-fig, ax = plt.subplots()
-model_names = ['Random Forest', 'Linear Regression']
-accuracy_values = [rf_accuracy, lr_accuracy]
-ax.bar(model_names, accuracy_values)
-ax.set_title('Accuracy Comparison')
-ax.set_xlabel('Model')
-ax.set_ylabel('Accuracy')
-st.pyplot(fig)
 
 
